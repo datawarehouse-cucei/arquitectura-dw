6e86b8fb3d112cb9ed29985441d6502b47a7d671
ReneGarBer <67211759+ReneGarBer@users.noreply.github.com>
2022-10-18 00:28:41 -0500

    funciones etl implementadas
    la funcon convert no se ha provado

--- stagin.py
+++ stagin.py
@@ -1,55 +1,54 @@
 import petl as etl
+import pandas as pd
 import psycopg2
+import re
 import os
+
+#funciones petl utilizadas fromcsv, head, tail, nrows, rowslice, appendcsv, todb, tohtml, tjson, convert
+
+#obtener direciones de la carpeta INEGI -- done
+
+ingei_paths = os.listdir("INEGI/")
+
+#Leer archivos para limpieza y exportar a csv --done
+
+column_headers =  ["Id","C_vehivulo_automotor","C_peaton","C_animal","C_objeto_fijo","Volcadura","Caida_de_pasajero","Salida_del_camino","Incendio","C_ferrocarril","C_motocicleta","C_ciclista","Otro"]
+anio = 2010
+
+for path in ingei_paths:
+    inegi_tbl = pd.read_excel("INEGI/"+path,skiprows=19,names=column_headers,header=None)
+    inegi_tbl.to_csv(f"INEGI/INEGI_{anio}.csv".format(anio))
+    anio += 1
+
+#Tomar el path de los archivos csv creado para leerlos con petl -- done
+inegi_file = os.listdir("INEGI/")
+inegi_csv = []
+for path in inegi_file:
+    if re.findall(".csv$",path):        
+        inegi_csv.append(path)
+
+#Leer archivo y revisar con las últimas filas y eliminar ultimas filas --done
+#Unir en un archivo csv
+
+os.mkdir("INEGI/INEGI_JOIN.csv")
+
+for path in inegi_csv:
+    tbl_inegi = etl.fromcsv("INEGI/"+path)
+    print(etl.head(tbl_inegi,4))
+    print(etl.tail(tbl_inegi,4))
+    nrows = etl.nrows(tbl_inegi)
+    tbl2_inegi = etl.rowslice(tbl_inegi,0,nrows-4)
+    etl.appendcsv(tbl2_inegi,"INEGI/INEGI_JOIN.csv")
+    print(etl.tail(tbl2_inegi,1))
+
+tbl_inegi_join = etl.fromcsv("INEGI/INEGI_JOIN.csv")
+
+#convierte un valor a flotante
+#tbl_inegi_join = etl.convert()
 conexion = psycopg2.connect(database="inegi", 
                             user="postgres", password="postgres",
                             host="localhost", port="5432")
 
-tbl_inegi2010 = etl.fromxlsx('INEGI/INEGI_2010.xlsx',sheet='hoja')
-
-print("INEGI_2010\n",tbl_inegi2010)
-
-tbl_headers_inegi_2010 = etl.header(tbl_inegi2010)
-
-print("Headers, INEGI_2010\n",tbl_headers_inegi_2010)
-
-tbl_headers_inegi_2010 = tbl_headers_inegi_2010[2:]
-
-print("INEGI_2010, after cut\n",tbl_headers_inegi_2010)
-
-tbl_inegi2010 = etl.cut(tbl_inegi2010,tbl_headers_inegi_2010)
-
-print(tbl_inegi2010)
-
-tbl_inegi2010 = etl.skip(tbl_inegi2010,2)
-
-print(tbl_inegi2010)
-
-tbl_inegi2011 = etl.fromxlsx('INEGI/INEGI_2011.xlsx',sheet='hoja')
-tbl_headers_inegi_2011 = etl.header(tbl_inegi2011)
-tbl_headers_inegi_2011 = tbl_headers_inegi_2011[2:]
-tbl_inegi2011 = etl.cut(tbl_inegi2011,tbl_headers_inegi_2011)
-
-tbl_inegi_union = etl.cat(tbl_inegi2010,tbl_inegi2011)
-
-print("UNION INEGI_2010, INEGI_2011\n",tbl_inegi_union)
-
-#Si es posible cambiar el formato de los archivos y eliminar algunas columnas, si no hacer manualmente
-
-#Para cada archivo INEGI, eliminar la primera columna y unir todos en una sola tabla
-
-#Crear tabla y mandar a la base de datos
-#Crear tabla vehiculos en base de datos inegi
-#insertar datos de archivo 'ParqueVehicular.xlsx'
-
-#Para los archivos RAMA
-
-#Crear una base de datos RAMA
-
-#Crear tablas CO, NO, NO2, NOX, O3, PM10, PM25, SO2
-
-#Leer archivos CO y unir con el resto de años.
-
-#Insertar en tabla RAMA.CO
-
-#Hacer lo mismo con el resto de archvos, NO, NO2, NOX, O3, PM10, PM25, SO2
+etl.todb(tbl_inegi_join, conexion, 'tabla', create=False)
+etl.tohtml(tbl_inegi_join, 'tabla.html',caption='Tabla de datos')
+etl.tojson(tbl_inegi_join, 'tabla.json')
