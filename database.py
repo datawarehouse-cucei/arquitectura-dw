bd1718f57357ce5e2fb3c1d1195dd13ed7ff7fba
ReneGarBer <67211759+ReneGarBer@users.noreply.github.com>
2022-10-22 13:39:10 -0500

    Update database.py

--- database.py
+++ database.py
@@ -3,12 +3,12 @@ import psycopg2
 
 config = dotenv_values('.env')
 
-conexion = psycopg2.connect(database=config['DATABASE'], 
-                            user=config['PASSWORD'], password=config['PASSWORD'],
-                            host=config['HOST'], port=config['PORT'])
+def set_conexion(db: str=config['DATABASE'], port:str=config['PORT'],
+                 user:str=config['PASSWORD'],password:str=config['PASSWORD'], host:str=config['HOST']):
+    conexion = psycopg2.connect(database=db, user=user, password=password, host=host, port=port)
+    return conexion
 
-cur = conexion.cursor()
-
-def table_exist(table:str):
+def table_exist(table:str,conexion):
+    cur = conexion.cursor()
     cur.execute(f"select * from information_schema.tables where table_name='{table}'")
     return bool(cur.rowcount)
